9c35c82b904e65dbbbefeaeb7fbff971273ac171
Stefan Hornburg (Racke) <racke@linuxia.de>
2013-12-26 12:08:39 +0100

    Update copyright years.

--- lib/Business/OnlinePayment/Braintree.pm
+++ lib/Business/OnlinePayment/Braintree.pm
@@ -214,7 +214,7 @@ Grant for the following enhancements (RT #88525):
 
 =head1 LICENSE AND COPYRIGHT
 
-Copyright 2011 Stefan Hornburg (Racke).
+Copyright 2011-2013 Stefan Hornburg (Racke).
 
 This program is free software; you can redistribute it and/or modify it
 under the terms of either: the GNU General Public License as published
